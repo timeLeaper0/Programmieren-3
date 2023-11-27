10981a3d6b1b1c9379ac4ac3816f803f5799f70b
timeLeaper0 <time.leaper@tumo.world>
2023-11-27 17:43:14 +0100

    Mit Fehlern

--- Abschlussprojekt_time.leaper/rasendestroyer.js
+++ Abschlussprojekt_time.leaper/rasendestroyer.js
@@ -1,10 +1,21 @@
 class RasenDestroyer {
   energie = 15;
-  zeile;
-  spalte;
-  platziereSelbstInMatrix() {
-      matrix[this.zeile][this.spalte] = 2;
-  };
+  // zeile;
+  // spalte;
+  
+  // platziereSelbstInMatrix() {
+  //     matrix[this.zeile][this.spalte] = 2;
+  // };
+
+//   constructor(z,s) {
+//     this.zeile = z;
+//     this.spalte = s;
+//     this.platziereSelbstInMatrix();
+// }
+constructor(z,s) {
+  super(z,s);
+  matrix[this.zeile][this.spalte] = 2;
+};
   spielzug() {
     if (this.energie > 30) {
       this.platzierNeuesObjekt();
@@ -17,58 +28,71 @@ class RasenDestroyer {
     }
   };
   platzierNeuesObjekt() {
-      let umgebung = [
-          [this.zeile + 1, this.spalte],
-          [this.zeile, this.spalte + 1],
-          [this.zeile - 1, this.spalte],
-          [this.zeile, this.spalte - 1],
-          // schräge Bewegung
-          [this.zeile + 1, this.spalte + 1],
-          [this.zeile - 1, this.spalte - 1],
-          [this.zeile - 1, this.spalte + 1],
-          [this.zeile+ 1, this.spalte - 1]
-      ];
-      let umgebungGefiltert = [];
-      for (let i = 0; i < 4; i++) {
-          let koordinate = umgebung[i]
-          // hier, überprüfe ob die koordinate außerhalb der Matrix liegt
-          if (koordinate[0] < 0 || koordinate[0] >= matrix.length || koordinate[1] < 0 || koordinate[1] >= matrix.length) {
+      // let umgebung = [
+      //     [this.zeile + 1, this.spalte],
+      //     [this.zeile, this.spalte + 1],
+      //     [this.zeile - 1, this.spalte],
+      //     [this.zeile, this.spalte - 1],
+      //     // schräge Bewegung
+      //     [this.zeile + 1, this.spalte + 1],
+      //     [this.zeile - 1, this.spalte - 1],
+      //     [this.zeile - 1, this.spalte + 1],
+      //     [this.zeile+ 1, this.spalte - 1]
+      // ];
+      // let umgebungGefiltert = [];
+      // for (let i = 0; i < 4; i++) {
+      //     let koordinate = umgebung[i]
+      //     // hier, überprüfe ob die koordinate außerhalb der Matrix liegt
+      //     if (koordinate[0] < 0 || koordinate[0] >= matrix.length || koordinate[1] < 0 || koordinate[1] >= matrix.length) {
   
-          } else if (matrix[koordinate[0]][koordinate[1]] === 0) {
-              umgebungGefiltert.push(koordinate);
-          }
-      };
+      //     } else if (matrix[koordinate[0]][koordinate[1]] === 0) {
+      //         umgebungGefiltert.push(koordinate);
+      //     }
+      // };
   
-      if(umgebungGefiltert.length > 0){
-        let koordinate = umgebungGefiltert[randomNumber(0,umgebungGefiltert.length)];
-          objekteArray.push(new RasenDestroyer(koordinate[0],koordinate[1]));
+      // if(umgebungGefiltert.length > 0){
+      //   let koordinate = umgebungGefiltert[randomNumber(0,umgebungGefiltert.length)];
+      //     objekteArray.push(new RasenDestroyer(koordinate[0],koordinate[1]));
+      // }
+      // return;
+
+      
+        let erdeListe = super.gefilterteUmgebung(0)
+      
+           if(erdeListe.length > 0){
+              let koordinate = erdeListe[randomNumber(0,erdeListe.length)];
+                objekteArray.push(new RasenDestroyer(koordinate[0],koordinate[1]));
+            }
+          return;
       }
-      return;
+      
   }
   machSchritt() {
-      let umgebung = [
-        [this.zeile + 1, this.spalte],
-        [this.zeile, this.spalte + 1],
-        [this.zeile - 1, this.spalte],
-        [this.zeile, this.spalte - 1],
-        [this.zeile + 1, this.spalte + 1],
-        [this.zeile + 1, this.spalte - 1],
-        [this.zeile - 1, this.spalte - 1],
-        [this.zeile - 1, this.spalte + 1]
-      ];
-      let umgebungGefiltert = [];
-      for (let i = 0; i < 8; i++) {
-          let koordinate = umgebung[i]
-          // hier, überprüfe ob die koordinate außerhalb der Matrix liegt
-          if (koordinate[0] < 0 || koordinate[0] >= matrix.length || koordinate[1] < 0 || koordinate[1] >= matrix.length) {
+      // let umgebung = [
+      //   [this.zeile + 1, this.spalte],
+      //   [this.zeile, this.spalte + 1],
+      //   [this.zeile - 1, this.spalte],
+      //   [this.zeile, this.spalte - 1],
+      //   [this.zeile + 1, this.spalte + 1],
+      //   [this.zeile + 1, this.spalte - 1],
+      //   [this.zeile - 1, this.spalte - 1],
+      //   [this.zeile - 1, this.spalte + 1]
+      // ];
+      // let umgebungGefiltert = [];
+      // for (let i = 0; i < 8; i++) {
+      //     let koordinate = umgebung[i]
+      //     // hier, überprüfe ob die koordinate außerhalb der Matrix liegt
+      //     if (koordinate[0] < 0 || koordinate[0] >= matrix.length || koordinate[1] < 0 || koordinate[1] >= matrix.length) {
   
-          } else if (matrix[koordinate[0]][koordinate[1]] === 1) {
-              umgebungGefiltert.push(koordinate);
-          }
-      };
+      //     } else if (matrix[koordinate[0]][koordinate[1]] === 1) {
+      //         umgebungGefiltert.push(koordinate);
+      //     }
+      // };
   
-      if(umgebungGefiltert.length > 0){
-        let koordinate = umgebungGefiltert[randomNumber(0,umgebungGefiltert.length)];
+      let grasListe = super.gefilterteUmgebung(1)
+
+      if(grasListe.length > 0){
+        let koordinate = grasListe[randomNumber(0,grasListe.length)];
         matrix[this.zeile][this.spalte] = 0;
           löschObjekt(koordinate[0],koordinate[1],objekteArray);
         this.zeile = koordinate[0];
@@ -81,9 +105,5 @@ class RasenDestroyer {
       }
       return;
   };
-  constructor(z,s) {
-      this.zeile = z;
-      this.spalte = s;
-      this.platziereSelbstInMatrix();
-  }
-  };
+  
+  // };
